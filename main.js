50cee847802feb826273961dda6ed69d74f881d2
Roy <roymeplease@gmail.com>
2020-03-16 16:27:08 +0200

    add the option to add tasks to the todo-list dynamically with the input section and the Add button

---
+++ main.js
@@ -0,0 +1,14 @@
+const input = document.querySelector(".todo-input");
+const addBtn = document.querySelector(".add-btn");
+const todoList = document.querySelector(".todo-list");
+const todoDiv = document.querySelector(".todo-item");
+
+addBtn.addEventListener('click', addTodo);
+
+function addTodo(event) {
+    event.preventDefault();
+    let newDiv = document.createElement("div");
+    newDiv = todoDiv.cloneNode(true);
+    newDiv.firstElementChild.textContent = input.value;
+    todoList.appendChild(newDiv);
+}
