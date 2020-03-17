9afee799abdfcafd1b006f3bd49f149093372494
Roy <roymeplease@gmail.com>
2020-03-17 21:41:05 +0200

    previously, if you pressed the REMOVE or DONE button on a task you just entered in the input section, it wouldn't work. Now, gave these dynmically entered tasks the events that occour on the original tasks.

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const todoRemove = document.querySelectorAll(".todo-remove");
 const todoDone = document.querySelectorAll(".todo-change-action");
 
 
+
 addBtn.addEventListener('click', addTodo);
 colorBtn.addEventListener('click', toggleColor);
 todoRemove.forEach(element => {
@@ -22,12 +23,18 @@ todoDone.forEach(element => {
 doneListBtn.addEventListener('click', moveToDoneList);
 todoListBtn.addEventListener('click', moveToTodoList);
 
+
+
 function addTodo(event) {
     event.preventDefault();
     let newDiv = document.createElement("div");
     newDiv = todoDiv.cloneNode(true);
     newDiv.firstElementChild.textContent = input.value;
     todoList.appendChild(newDiv);
+    let getDoneBtn = newDiv.lastElementChild;
+    getDoneBtn.addEventListener('click', doneTodoItem);
+    const theRemoveBtn = getRemoveBtn(newDiv);
+    theRemoveBtn.addEventListener('click',removeTodoItem);
     input.value = ''
 }
 
@@ -35,16 +42,32 @@ function toggleColor(event) {
     event.preventDefault();
     if (todoList.style.backgroundColor === 'white') {
         todoList.style.backgroundColor = 'grey';
+        doneList.style.backgroundColor = 'grey';
     } else {
         todoList.style.backgroundColor = 'white';
+        doneList.style.backgroundColor = 'white';
     }
 }
 
 function removeTodoItem(event) {
     event.preventDefault();
     const thisDiv = this.parentElement
+
     thisDiv.remove();
 }
+
+function getRemoveBtn(nodeClone) {
+    let removeBtn;
+    nodeClone.childNodes.forEach(function (element) {
+        if (element.nodeName !== "BUTTON") {
+        }
+        else if (element.getAttribute("class").includes("todo-remove")) {
+            removeBtn = element
+        }
+    });
+    return removeBtn;
+}
+
 
 function doneTodoItem(event) {
     event.preventDefault();
@@ -53,10 +76,10 @@ function doneTodoItem(event) {
     let lastElementChild = parentElement.lastElementChild;
     firstElementChild.style.textDecoration = 'line-through';
     lastElementChild.style.display = 'none';
-    doneList.appendChild(parentElement)
+    doneList.appendChild(parentElement);
 }
 
-function moveToDoneList (event) {
+function moveToDoneList(event) {
     event.preventDefault();
     doneList.style.display = 'flex';
     doneListBtn.style.backgroundColor = '#8cb691'
